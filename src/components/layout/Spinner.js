704264eddd73877d3aea4558e23ddaefc2074360
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-08-09 21:32:07 +0100

    initial commit

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import spinner from './spinner.gif'
+
+const Spinner = () => {
+    return (
+        <Fragment>
+            <img src={spinner} alt="Loading..." style={{ display: 'block', width: '200px', margin: 'auto'}}/>
+        </Fragment>
+    )
+}
+
+export default Spinner
